1fe7adcbfbeb902f7d89a354dd2ac21432159a26
VMoreno158 <vmorenode.inf@upsa.es>
2025-11-04 15:59:56 +0100

    commit start

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Hero } from "./components/hero/hero";
+import { Header } from "./components/header/header";
+import { Proyects } from "./components/proyects/proyects";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Hero, Header, Proyects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
